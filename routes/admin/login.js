881f9efc50f451c9b26201ac322e45a83ab564d4
alivLG <486178631@qq.com>
2018-06-21 23:53:33 +0800

    *）链接数据库验证登录信息

--- routes/admin/login.js
+++ routes/admin/login.js
@@ -1,13 +1,26 @@
 var Router = require('koa-router');
 var tools = require('../../model/tools');
+var DB = require('../../model/db');
 var router = new Router();
 
 router.get("/", async (ctx) => {
   await ctx.render("admin/login");
 });
 router.post("/doLogin", async (ctx) => {
-  console.log(ctx.request.body);
   let username = ctx.request.body.username;
-  let password = tolls.md5(ctx.request.body.password);
+  let password = ctx.request.body.password;
+  //匹配数据库 验证账号密码
+  var result = await DB.find("admin", {
+    "username": username,
+    "password": tools.md5(password)
+  })
+  console.log(result)
+  if (result.length > 0) {
+    console.log("登录成功！");
+    ctx.session.userinfo = result[0];
+    ctx.redirect(ctx.state.__HOST__ + "/admin")
+  } else {
+    console.log("登录失败")
+  }
 })
 module.exports = router.routes();

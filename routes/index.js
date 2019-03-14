7fa38d98a8228e218bb2bb5229e67414d1447f27
alivnLG <486178631@qq.com>
2019-03-14 16:38:13 +0800

    *)测试

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var Router = require('koa-router');
 var router = new Router();
 
 router.get("/", async (ctx) => {
-    ctx.body = "前台首页";
+    ctx.body = "前台首页---";
 });
 
 module.exports = router.routes();
